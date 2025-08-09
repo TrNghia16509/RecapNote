a916de37dad1a80fb3ed2ba57f948057b01b7c05
TrNghia16509 <101879831+TrNghia16509@users.noreply.github.com>
2025-08-10 02:18:55 +0700

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -300,7 +300,7 @@ st.subheader("🎙 Ghi âm trực tiếp bằng React-Mic")
 
 # 1. Khai báo component
 _component_dir = os.path.join(os.path.dirname(__file__), "st_react_mic", "frontend", "build")
-st_react_mic = components.declare_component("st_react_mic", path=_component_dir)
+st_react_mic = components.declare_component("frontend", path=_component_dir)
 st.write("📂 Component path:", _component_dir, os.path.exists(_component_dir))
 
 # 2. Hiển thị component
