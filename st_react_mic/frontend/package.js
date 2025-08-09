0a324a4e3d01e93e76d2905d2dfbfb4ea7a23179
TrNghia16509 <101879831+TrNghia16509@users.noreply.github.com>
2025-08-09 22:59:23 +0700

    Create package.js

---
+++ st_react_mic/frontend/package.js
@@ -0,0 +1,15 @@
+{
+  "name": "st_react_mic_frontend",
+  "version": "1.0.0",
+  "private": true,
+  "dependencies": {
+    "react": "^18.2.0",
+    "react-dom": "^18.2.0",
+    "react-mic": "^12.5.1",
+    "streamlit-component-lib": "^1.5.0"
+  },
+  "scripts": {
+    "start": "react-scripts start",
+    "build": "react-scripts build"
+  }
+}
